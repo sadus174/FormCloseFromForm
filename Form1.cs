ce4d7f2928c0c8b1d9be58532991ae8dcc4a1c64
sadus174 <34853375+sadus174@users.noreply.github.com>
2020-02-28 17:12:16 +0500

    1

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,26 @@ namespace WindowsFormsApp2
 {
     public partial class Form1 : Form
     {
+        Form2 form2; //добавляем поле класса дочерней формы
+
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e) //кнопка "Открыть"
+        {
+            form2 = new Form2();
+            form2.Show(); //открываем
+        }
+        private void button2_Click(object sender, EventArgs e) // кнопка "Закрыть"
+        {
+            if (form2 != null) //если ссылка не пустая
+            {
+                form2.Close(); //закрываем
+            }
+        }
     }
+    
 }
---
+++ Form2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        public Button but1; // эта переменная будет содержать ссылку на кнопку button1 из формы Form1
+
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
